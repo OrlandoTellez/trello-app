0ba5927bb3d973733d63cc7d27c21b1fab0527f8
Orlando <espadachinkinno@gmail.com>
2025-01-29 22:24:30 -0600

    Logica de renderizas listas y apartirde ellas renderizar las cards

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-const addCart = document.querySelector(".add-card")
-const containerInput = document.querySelector(".container-input")
-const columna = document.querySelector(".column")
-const containerEdit = document.querySelector(".container-edit")
 const btnLista = document.querySelector(".btn-agregar--lista")
 const tablero = document.querySelector(".board")
 
@@ -9,59 +5,49 @@ btnLista.addEventListener("click", () => {
     renderLista()
 })
 
-addCart.addEventListener("click", () => {
-    addCart.style.display = "none"
-    renderFormCreate()
-    eventoForm()
-});
-
-function renderLista(){
+function renderLista() {
     const div = document.createElement("div")
     div.classList.add("column")
     div.innerHTML = `
-    <h2>To do</h2>
+        <h2>To do</h2>
         <div class="container-edit"></div>
         <div class="container-input"></div>
-    <button class="add-card">Add card</button>
+        <button class="add-card">Add card</button>
     `
     tablero.appendChild(div)
 
-    
+    const containerEdit = div.querySelector(".container-edit")
+    const containerInput = div.querySelector(".container-input")
+    const addCart = div.querySelector(".add-card")
+
+    addCart.addEventListener("click", () => {
+        addCart.style.display = "none"
+        renderFormCreate(containerInput, addCart)
+    })
+
+    div.dataset.columnRef = true
 }
 
-function renderFormCreate() {
-    return (containerInput.innerHTML = `
-    <form class="form-crear--tarjeta">
-        <input type="text" class="input-agregar--tarjeta" placeholder="Agregue una nueva tarea...">
-        <textarea class="text-area--agregar" placeholder="Agregue una descripción"></textarea>
-        <div>
-            <button type="submit" class="btn-crear--tarjeta">Añadir tarjeta</button>
-            <button type="button" class="btn-cancelar">Cancelar</button>
-        </div>
-    </form>
-    `)
-}
-
-function renderFormEdit(titulo, descripcion) {
-    return (containerEdit.innerHTML = `
-        <form class="form-edit--tarjeta">
-            <div class="edit">
-                <input class="input-tarea--edit" type="text" value="${titulo}" placeholder="Edite el titulo">
-                <textarea class="text-area--descripcion" placeholder="Agregue una descripcion">${descripcion}</textarea>
-            </div>
-            <div class="buttons-edit">
-                <button type="submit" class="btn-guardar--cambios">Save</button>
-                <button type="button" class="btn-cancelar">Cancel</button>
+function renderFormCreate(containerInput, btnAddCard) {
+    containerInput.innerHTML = `
+        <form class="form-crear--tarjeta">
+            <input type="text" class="input-agregar--tarjeta" placeholder="Agregue una nueva tarea...">
+            <textarea class="text-area--agregar" placeholder="Agregue una descripción"></textarea>
+            <div>
+                <button type="submit" class="btn-crear--tarjeta">Añadir tarjeta</button>
+                <button type="button" class="btn-cancelar">Cancelar</button>
             </div>
         </form>
-    `)
+    `
+
+    eventoForm(containerInput, btnAddCard)
 }
 
-function eventoForm() {
-    const formCrearTarjeta = document.querySelector(".form-crear--tarjeta")
-    const btnCancelar = document.querySelector(".btn-cancelar")
-    const inputAgregarTarjeta = document.querySelector(".input-agregar--tarjeta")
-    const textAreaAgregar = document.querySelector(".text-area--agregar")
+function eventoForm(containerInput, btnAddCard) {
+    const formCrearTarjeta = containerInput.querySelector(".form-crear--tarjeta")
+    const btnCancelar = containerInput.querySelector(".btn-cancelar")
+    const inputAgregarTarjeta = containerInput.querySelector(".input-agregar--tarjeta")
+    const textAreaAgregar = containerInput.querySelector(".text-area--agregar")
 
     formCrearTarjeta.addEventListener("submit", (e) => {
         e.preventDefault()
@@ -69,9 +55,9 @@ function eventoForm() {
         const descripcion = textAreaAgregar.value.trim()
 
         if (tarea) {
-            renderCard(tarea, descripcion)
+            renderCard(tarea, descripcion, containerInput.closest(".column"))
             containerInput.innerHTML = ""
-            addCart.style.display = "block"
+            btnAddCard.style.display = "block"
         } else {
             alert("Por favor escriba una tarea")
         }
@@ -79,11 +65,11 @@ function eventoForm() {
 
     btnCancelar.addEventListener("click", () => {
         containerInput.innerHTML = ""
-        addCart.style.display = "block"
-    });
+        btnAddCard.style.display = "block"
+    })
 }
 
-function renderCard(tarea, descripcion = "") {
+function renderCard(tarea, descripcion, columna) {
     const nuevaTarjeta = document.createElement("div")
     nuevaTarjeta.classList.add("card")
     nuevaTarjeta.innerHTML = `
@@ -96,39 +82,53 @@ function renderCard(tarea, descripcion = "") {
             <img class="icono-borrar" src="/assets/icons/trash.svg" alt="delete-icon">
         </div>
     `
-    
+
     const iconoEditar = nuevaTarjeta.querySelector(".icono-editar")
     const iconoBorrar = nuevaTarjeta.querySelector(".icono-borrar")
-    const tituloTareaContainer = nuevaTarjeta.querySelector(".titulo-tarea")
-    const descripcionTareaContainer = nuevaTarjeta.querySelector(".descripcion-tarea")
 
     iconoBorrar.addEventListener("click", () => {
         columna.removeChild(nuevaTarjeta)
-    });
+    })
 
     iconoEditar.addEventListener("click", () => {
         nuevaTarjeta.style.display = "none"
-        renderFormEdit(tituloTareaContainer.textContent, descripcionTareaContainer.textContent)
-
-        const formularioEdit = document.querySelector(".form-edit--tarjeta")
-        const inputTarea = document.querySelector(".input-tarea--edit")
-        const textAreaDescripcion = document.querySelector(".text-area--descripcion")
-        const btnCancelar = document.querySelector(".btn-cancelar")
-
-        formularioEdit.addEventListener("submit", (e) => {
-            e.preventDefault()
-            containerEdit.innerHTML = ""
-            tituloTareaContainer.textContent = inputTarea.value
-            descripcionTareaContainer.textContent = textAreaDescripcion.value
-            nuevaTarjeta.style.display = "flex"
-        });
-
-        btnCancelar.addEventListener("click", () => {
-            containerEdit.innerHTML = ""
-            nuevaTarjeta.style.display = "flex"
-        });
-    });
+        renderFormEdit(tarea, descripcion, columna, nuevaTarjeta)
+    })
 
     columna.appendChild(nuevaTarjeta)
 }
 
+function renderFormEdit(titulo, descripcion, columna, tarjeta) {
+    const containerEdit = columna.querySelector(".container-edit")
+
+    containerEdit.innerHTML = `
+        <form class="form-edit--tarjeta">
+            <div class="edit">
+                <input class="input-tarea--edit" type="text" value="${titulo}" placeholder="Edite el título">
+                <textarea class="text-area--descripcion" placeholder="Agregue una descripción">${descripcion}</textarea>
+            </div>
+            <div class="buttons-edit">
+                <button type="submit" class="btn-guardar--cambios">Save</button>
+                <button type="button" class="btn-cancelar">Cancel</button>
+            </div>
+        </form>
+    `
+
+    const formularioEdit = containerEdit.querySelector(".form-edit--tarjeta")
+    const inputTarea = containerEdit.querySelector(".input-tarea--edit")
+    const textAreaDescripcion = containerEdit.querySelector(".text-area--descripcion")
+    const btnCancelar = containerEdit.querySelector(".btn-cancelar")
+
+    formularioEdit.addEventListener("submit", (e) => {
+        e.preventDefault()
+        containerEdit.innerHTML = ""
+        tarjeta.querySelector(".titulo-tarea").textContent = inputTarea.value
+        tarjeta.querySelector(".descripcion-tarea").textContent = textAreaDescripcion.value
+        tarjeta.style.display = "flex"
+    })
+
+    btnCancelar.addEventListener("click", () => {
+        containerEdit.innerHTML = ""
+        tarjeta.style.display = "flex"
+    })
+}
